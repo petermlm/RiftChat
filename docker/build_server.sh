69b0e850694eda1318b12e1406fce027b35cc796
Pedro Melgueira <pedromelgueira@gmail.com>
2017-02-16 16:32:58 +0000

    Made modification to Docker builds

---
+++ docker/build_server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+if [ $# -eq 1 ]
+then
+    conf_file=$1
+else
+    conf_file=../src/confs/rift_server.conf
+fi
+
+mkdir -p confs
+cp $conf_file confs/rift_server.conf
+
+sudo docker build -t rift-chat-server -f Dockerfile.server ..
